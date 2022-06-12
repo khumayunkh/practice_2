9f93d888bf9a97699da18f7d749b01ca8a57cc82
khumayunkh <hasanovhumayun15@gmail.com>
2022-06-12 22:12:39 +0500

    map/set

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log('hello')
+
+let x = [1,2,3,4,5]
+let sum = 0
+for(let i of x){
+    sum += i
+}
+
+console.log(sum)
+
+let euro = Intl.NumberFormat('es',{style:'currency', currency:'EUR'}).format(18);
+console.log(euro)
+
+console.log('hello')
+
+let s = new Set();
+s.add(2).add(2).add(3) // 2, 3
+console.log(s)
+
+let m = new Map()
+m.set('one', 2).set('two',2)
+console.log(m.get('one')) // 1
+console.log(m.get(2)) // undefined
